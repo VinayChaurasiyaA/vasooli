81165f53d5a26ea1a41a3442d630cf5d44551033
VINAY CHAURASIYA <vinaychaurasiya73@gmail.com>
2023-06-21 12:40:40 +0530

    vasooli-on-his-way

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { GrFormAdd } from "react-icons/gr";
+import { data } from "./data";
+import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState(data);
+  const [total, setTotal] = useState(0);
+
+  const addToValue = (id) => {
+    // setItems((prevItems) => {
+    //   return prevItems.map((item) => {
+    //     if (item.id === id) {
+    //       return { ...item, value: item.value + 1 };
+    //     }
+    //     return item;
+    //   });
+    // });
+    setItems((prevItems) => {
+      return prevItems.map((item) =>
+        item.id === id ? { ...item, value: item.value + 1 } : item
+      );
+    });
+  };
+  const totalValue = () => {
+    let temp = 0;
+    const value = items.map((item) => (temp += item.value * item.price));
+    // const totalValue = items.reduce((acc, curr) => {
+    //   return acc + curr.price * curr.value;
+    // }, 0);
+    setTotal(temp);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bbq- Vasooli</h1>
+      <main className="menu">
+        <div>
+          {items.map((item) => {
+            return (
+              <div key={item.id} className="container">
+                <img className="img" src={item.image} alt={item.name} />
+                <h3>{item.name}</h3>
+                <h3 className="shift">price: {item.price}</h3>
+                <button className="btn-add" onClick={() => addToValue(item.id)}>
+                  <GrFormAdd />
+                  {item.value}
+                </button>
+              </div>
+            );
+          })}
+        </div>
+        <button className="btn-submit" onClick={totalValue}>
+          Total {total}
+        </button>
+      </main>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const data = [
+  {
+    id: 1,
+    name: "Chicken Tikka",
+    price: 100,
+    image:
+      "https://t4.ftcdn.net/jpg/02/42/39/29/240_F_242392945_5Xc5VjsBFUMFR3ZZdQV1FL6LgB0XIvhD.jpg",
+    value: 0,
+  },
+  {
+    id: 2,
+    name: "Chicken Leg Piece",
+    price: 150,
+    image:
+      "https://t4.ftcdn.net/jpg/06/00/54/47/240_F_600544722_xDkmh5XricH04GWJJZhkQwdJxmxf1u0Y.jpg",
+    value: 0,
+  },
+  {
+    id: 3,
+    name: "Prawns",
+    price: 150,
+    image:
+      "https://t3.ftcdn.net/jpg/06/00/55/82/240_F_600558259_Lxym6RyS6GV2R4vHEGK7TAlBVNN7m88C.jpg",
+    value: 0,
+  },
+  {
+    id: 4,
+    name: "Fish",
+    price: 150,
+    image:
+      "https://t4.ftcdn.net/jpg/01/08/43/45/240_F_108434598_IOHkNeZaswpGT5eyLSmGpHIIhvDlfpwo.jpg",
+    value: 0,
+  },
+  {
+    id: 5,
+    name: "Chicken kabab",
+    price: 55,
+    image:
+      "https://t3.ftcdn.net/jpg/03/09/16/98/240_F_309169810_wTEpSZQlQbIXLb7BV5fGBbuGvKH2cCZO.jpg",
+    value: 0,
+  },
+  {
+    id: 6,
+    name: "Mutton kabab",
+    price: 75,
+    image:
+      "https://t3.ftcdn.net/jpg/02/06/94/00/240_F_206940051_WdhOBF4bzgq9DRKDFUcXHOwAWQePqcrC.jpg",
+    value: 0,
+  },
+];
